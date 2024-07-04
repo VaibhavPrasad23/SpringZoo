cd912241ff177ddf7ea06d04a73ff61fc9e09ecc
VaibhavPrasad23”git config --global user.name VaibhavPrasad23” <vaibhav.prasad2@gmail.com>
2024-07-04 17:39:07 +0530

    new file:   .classpath 	new file:   .factorypath 	deleted:    .gitignore 	new file:   .idea/.name 	new file:   .idea/misc.xml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	new file:   .project 	new file:   .settings/org.eclipse.core.resources.prefs 	new file:   .settings/org.eclipse.jdt.apt.core.prefs 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   .settings/org.eclipse.m2e.core.prefs 	new file:   .vscode/settings.json 	new file:   target/ZooSpring-0.0.1-SNAPSHOT.jar 	new file:   target/ZooSpring-0.0.1-SNAPSHOT.jar.original 	new file:   target/classes/META-INF/MANIFEST.MF 	new file:   target/classes/META-INF/maven/com.example/ZooSpring/pom.properties 	new file:   target/classes/META-INF/maven/com.example/ZooSpring/pom.xml 	new file:   target/classes/application.properties 	new file:   target/classes/com/ics/springnuxt/ICS_Zoo$1.class 	new file:   target/classes/com/ics/springnuxt/ICS_Zoo.class 	new file:   target/classes/com/ics/springnuxt/controller/AppController.class 	new file:   target/classes/com/ics/springnuxt/controller/CarController.class 	new file:   target/classes/com/ics/springnuxt/controller/UsedController.class 	new file:   target/classes/com/ics/springnuxt/controller/UserController.class 	new file:   target/classes/com/ics/springnuxt/dto/CreateCarDto.class 	new file:   target/classes/com/ics/springnuxt/dto/CreateUserDto.class 	new file:   target/classes/com/ics/springnuxt/dto/ShowroomDto.class 	new file:   target/classes/com/ics/springnuxt/dto/UpdateCarDto.class 	new file:   target/classes/com/ics/springnuxt/dto/UpdateUserDto.class 	new file:   target/classes/com/ics/springnuxt/dto/UsedCarDto.class 	new file:   target/classes/com/ics/springnuxt/dto/UserDto.class 	new file:   target/classes/com/ics/springnuxt/entity/Car$CarBuilder.class 	new file:   target/classes/com/ics/springnuxt/entity/Car.class 	new file:   target/classes/com/ics/springnuxt/entity/UsedCar.class 	new file:   target/classes/com/ics/springnuxt/entity/User$UserBuilder.class 	new file:   target/classes/com/ics/springnuxt/entity/User.class 	new file:   target/classes/com/ics/springnuxt/mapper/DTOToObjectMapper.class 	new file:   target/classes/com/ics/springnuxt/mapper/DTOToObjectMapperImpl.class 	new file:   target/classes/com/ics/springnuxt/repository/CarRepository.class 	new file:   target/classes/com/ics/springnuxt/repository/UsedCarRepository.class 	new file:   target/classes/com/ics/springnuxt/repository/UserRepository.class 	new file:   target/classes/com/ics/springnuxt/service/CarService.class 	new file:   target/classes/com/ics/springnuxt/service/UsedCarService.class 	new file:   target/classes/com/ics/springnuxt/service/UserService.class 	new file:   target/generated-sources/annotations/com/ics/springnuxt/mapper/DTOToObjectMapperImpl.java 	new file:   target/maven-archiver/pom.properties 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

---
+++ target/generated-sources/annotations/com/ics/springnuxt/mapper/DTOToObjectMapperImpl.java
@@ -0,0 +1,170 @@
+package com.ics.springnuxt.mapper;
+
+import com.ics.springnuxt.dto.CreateCarDto;
+import com.ics.springnuxt.dto.CreateUserDto;
+import com.ics.springnuxt.dto.UpdateCarDto;
+import com.ics.springnuxt.entity.Car;
+import com.ics.springnuxt.entity.UsedCar;
+import com.ics.springnuxt.entity.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-03T12:01:16+0530",
+    comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240215-1558, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+@Component
+public class DTOToObjectMapperImpl implements DTOToObjectMapper {
+
+    @Override
+    public Car car(CreateCarDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Car.CarBuilder car = Car.builder();
+
+        car.brand( dto.getBrand() );
+        car.description( dto.getDescription() );
+        car.fuel( dto.isFuel() );
+        car.name( dto.getName() );
+        car.pic( dto.getPic() );
+        car.price( dto.getPrice() );
+        car.type( dto.getType() );
+        car.year( dto.getYear() );
+
+        return car.build();
+    }
+
+    @Override
+    public User user(CreateUserDto UserDTO) {
+        if ( UserDTO == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.email( UserDTO.getEmail() );
+        user.password( UserDTO.getPassword() );
+        user.username( UserDTO.getUsername() );
+
+        return user.build();
+    }
+
+    @Override
+    public UsedCar usedCar(CreateCarDto UsedCarDTO) {
+        if ( UsedCarDTO == null ) {
+            return null;
+        }
+
+        UsedCar usedCar = new UsedCar();
+
+        usedCar.setAddress( UsedCarDTO.getAddress() );
+        usedCar.setBrand( UsedCarDTO.getBrand() );
+        usedCar.setDescription( UsedCarDTO.getDescription() );
+        usedCar.setFuel( UsedCarDTO.isFuel() );
+        usedCar.setName( UsedCarDTO.getName() );
+        usedCar.setOwnername( UsedCarDTO.getOwnername() );
+        usedCar.setOwnernum( UsedCarDTO.getOwnernum() );
+        usedCar.setPic( UsedCarDTO.getPic() );
+        usedCar.setPrice( UsedCarDTO.getPrice() );
+        usedCar.setType( UsedCarDTO.getType() );
+        usedCar.setYear( UsedCarDTO.getYear() );
+
+        return usedCar;
+    }
+
+    @Override
+    public UsedCar updateCarToUsedCar(Car dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        UsedCar usedCar = new UsedCar();
+
+        usedCar.setBrand( dto.getBrand() );
+        usedCar.setName( dto.getName() );
+        usedCar.setDescription( dto.getDescription() );
+        usedCar.setFuel( dto.getFuel() );
+        usedCar.setPic( dto.getPic() );
+        usedCar.setPrice( dto.getPrice() );
+        usedCar.setType( dto.getType() );
+        usedCar.setYear( dto.getYear() );
+
+        return usedCar;
+    }
+
+    @Override
+    public UsedCar updateUsedCar(UpdateCarDto updateCarDTO) {
+        if ( updateCarDTO == null ) {
+            return null;
+        }
+
+        UsedCar usedCar = new UsedCar();
+
+        usedCar.setAddress( updateCarDTO.getAddress() );
+        usedCar.setBrand( updateCarDTO.getBrand() );
+        usedCar.setDescription( updateCarDTO.getDescription() );
+        usedCar.setName( updateCarDTO.getName() );
+        usedCar.setOwnername( updateCarDTO.getOwnername() );
+        usedCar.setOwnernum( updateCarDTO.getOwnernum() );
+        usedCar.setPic( updateCarDTO.getPic() );
+        usedCar.setPrice( updateCarDTO.getPrice() );
+        usedCar.setType( updateCarDTO.getType() );
+        usedCar.setYear( updateCarDTO.getYear() );
+
+        return usedCar;
+    }
+
+    @Override
+    public void updateCarFromDto(UpdateCarDto dto, UsedCar entity) {
+        if ( dto == null ) {
+            return;
+        }
+
+        entity.setAddress( dto.getAddress() );
+        entity.setBrand( dto.getBrand() );
+        entity.setDescription( dto.getDescription() );
+        entity.setFuel( dto.getFuel() );
+        entity.setName( dto.getName() );
+        entity.setOwnername( dto.getOwnername() );
+        entity.setOwnernum( dto.getOwnernum() );
+        entity.setPic( dto.getPic() );
+        entity.setPrice( dto.getPrice() );
+        entity.setType( dto.getType() );
+        entity.setYear( dto.getYear() );
+    }
+
+    @Override
+    public void updateCarFromDto(UpdateCarDto dto, Car entity) {
+        if ( dto == null ) {
+            return;
+        }
+
+        entity.setBrand( dto.getBrand() );
+        entity.setDescription( dto.getDescription() );
+        entity.setFuel( dto.getFuel() );
+        entity.setName( dto.getName() );
+        entity.setPic( dto.getPic() );
+        entity.setPrice( dto.getPrice() );
+        entity.setType( dto.getType() );
+        entity.setYear( dto.getYear() );
+    }
+
+    @Override
+    public void updateCarFromUsedCarDto(UpdateCarDto dto, Car car) {
+        if ( dto == null ) {
+            return;
+        }
+
+        car.setBrand( dto.getBrand() );
+        car.setDescription( dto.getDescription() );
+        car.setFuel( dto.getFuel() );
+        car.setName( dto.getName() );
+        car.setPic( dto.getPic() );
+        car.setPrice( dto.getPrice() );
+        car.setType( dto.getType() );
+        car.setYear( dto.getYear() );
+    }
+}
